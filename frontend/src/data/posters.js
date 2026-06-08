e3de41929089c97780a7c9ffaf5149a7b65bf477
ESTURRARI <esturrarigabi@gmail.com>
2026-06-08 08:52:53 -0300

    Integra listagem de filmes com API

---
+++ frontend/src/data/posters.js
@@ -0,0 +1,53 @@
+import venom from '../assets/posters/venom.png'
+import thor from '../assets/posters/thor.png'
+import deadpool from '../assets/posters/deadpool.png'
+import spiderman3 from '../assets/posters/spiderman3.png'
+import bighero from '../assets/posters/bighero.png'
+import black from '../assets/posters/black.png'
+import castelo from '../assets/posters/castelo.png'
+import chainsaw from '../assets/posters/chainsaw.png'
+import chihiro from '../assets/posters/chihiro.png'
+import coraline from '../assets/posters/coraline.png'
+import demon from '../assets/posters/demon.png'
+import elementos from '../assets/posters/elementos.png'
+import jack from '../assets/posters/jack.png'
+import jujutsu from '../assets/posters/jujutsu.png'
+import marnie from '../assets/posters/marnie.png'
+import monstros from '../assets/posters/monstros.png'
+import mufasa from '../assets/posters/mufasa.png'
+import noiva from '../assets/posters/noiva.png'
+import ratatouille from '../assets/posters/ratatouille.png'
+import tokyo from '../assets/posters/tokyo.png'
+import up from '../assets/posters/up.png'
+import urso from '../assets/posters/urso.png'
+import viuva from '../assets/posters/viuva.png'
+import walle from '../assets/posters/walle.png'
+import zootopia2 from '../assets/posters/zootopia2.png'
+
+export const posters = {
+  'venom.png': venom,
+  'thor.png': thor,
+  'deadpool.png': deadpool,
+  'spiderman3.png': spiderman3,
+  'bighero.png': bighero,
+  'black.png': black,
+  'castelo.png': castelo,
+  'chainsaw.png': chainsaw,
+  'chihiro.png': chihiro,
+  'coraline.png': coraline,
+  'demon.png': demon,
+  'elementos.png': elementos,
+  'jack.png': jack,
+  'jujutsu.png': jujutsu,
+  'marnie.png': marnie,
+  'monstros.png': monstros,
+  'mufasa.png': mufasa,
+  'noiva.png': noiva,
+  'ratatouille.png': ratatouille,
+  'tokyo.png': tokyo,
+  'up.png': up,
+  'urso.png': urso,
+  'viuva.png': viuva,
+  'walle.png': walle,
+  'zootopia2.png': zootopia2
+}
--- frontend/src/pages/filmes/filmes.jsx
+++ frontend/src/pages/filmes/filmes.jsx
@@ -1,42 +1,29 @@
 import './filmes.css'
 
-import venom from '../../assets/posters/venom.png'
-import deadpool from '../../assets/posters/deadpool.png'
-import thor from '../../assets/posters/thor.png'
-import viuva from '../../assets/posters/viuva.png'
-import spiderman3 from '../../assets/posters/spiderman3.png'
-
-import bighero from '../../assets/posters/bighero.png'
-import urso from '../../assets/posters/urso.png'
-import zootopia from '../../assets/posters/zootopia2.png'
-import mufasa from '../../assets/posters/mufasa.png'
-import estranho from '../../assets/posters/jack.png'
-
-import ratatouille from '../../assets/posters/ratatouille.png'
-import elementos from '../../assets/posters/elementos.png'
-import up from '../../assets/posters/up.png'
-import walle from '../../assets/posters/walle.png'
-import monstros from '../../assets/posters/monstros.png'
-
-import marnie from '../../assets/posters/marnie.png'
-import coraline from '../../assets/posters/coraline.png'
-import chihiro from '../../assets/posters/chihiro.png'
-import noiva from '../../assets/posters/noiva.png'
-import castelo from '../../assets/posters/castelo.png'
-
-import black from '../../assets/posters/black.png'
-import jujutsu from '../../assets/posters/jujutsu.png'
-import demon from '../../assets/posters/demon.png'
-import tokyo from '../../assets/posters/tokyo.png'
-import chainsaw from '../../assets/posters/chainsaw.png'
-
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { posters } from '../../data/posters'
 
+import { MdKeyboardArrowDown } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
 
 function Filmes(){
+
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(() => {
+
+    fetch('http://localhost:8000/listagem')
+      .then(response => response.json())
+      .then(data => {
+        setFilmes(data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+
+  }, [])
 
   return(
 
@@ -93,114 +80,26 @@ function Filmes(){
             <h2>Todos os Filmes</h2>
 
             <Link to="/adicionar" className="btn-adicionar">
-
               Adicionar +
-
             </Link>
 
           </div>
 
           <div className="linha-posters">
 
-            <Link to="/detalhes">
-              <img src={venom} alt="" />
-            </Link>
+            {filmes.map((filme) => (
 
-            <Link to="/detalhes">
-              <img src={deadpool} alt="" />
-            </Link>
+              <Link
+                key={filme.id}
+                to={`/detalhes/${filme.id}`}
+              >
+                <img
+                  src={posters[filme.imagem]}
+                  alt={filme.titulo}
+                />
+              </Link>
 
-            <Link to="/detalhes">
-              <img src={thor} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={viuva} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={spiderman3} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={bighero} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={urso} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={zootopia} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={mufasa} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={estranho} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={ratatouille} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={elementos} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={monstros} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={up} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={walle} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={coraline} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={chihiro} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={noiva} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={castelo} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={marnie} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={black} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={jujutsu} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={demon} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={tokyo} alt="" />
-            </Link>
-
-            <Link to="/detalhes">
-              <img src={chainsaw} alt="" />
-            </Link>
+            ))}
 
           </div>
 
